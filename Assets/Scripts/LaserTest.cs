b963c882a1af40afc5ae6b63ab4b2c555176c343
peke2 <abem@dc4.so-net.ne.jp>
2019-02-27 01:13:40 +0900

    中央と下地で分けたスプライトを同じ関数で処理するよう整理。 あまり整理されていない。

--- Assets/Scripts/LaserTest.cs
+++ Assets/Scripts/LaserTest.cs
@@ -9,15 +9,22 @@ public class LaserTest : MonoBehaviour {
 		Head,
 		Body,
 		Tail,
+		Max
+	}
 
-		CenterHead,
-		CenterBody,
-		CenterTail,
+	enum CenterParts
+	{
+		Head,
+		Body,
+		Tail,
 		Max
 	}
 
 	GameObject[] m_laserParts = new GameObject[(int)Parts.Max];
 	Sprite[] m_sprites = new Sprite[(int)Parts.Max];
+
+	GameObject[] m_laserCenterParts = new GameObject[(int)CenterParts.Max];
+	Sprite[] m_centerSprites = new Sprite[(int)CenterParts.Max];
 
 	const int LaserLenMax = 16;
 	GameObject[] m_laser = new GameObject[LaserLenMax];
@@ -43,19 +50,19 @@ public class LaserTest : MonoBehaviour {
 		m_sprites[(int)Parts.Body] = System.Array.Find<Sprite>(sprites, sprite => sprite.name == "body");
 		m_sprites[(int)Parts.Tail] = System.Array.Find<Sprite>(sprites, sprite => sprite.name == "tail");
 
-		m_sprites[(int)Parts.CenterHead] = System.Array.Find<Sprite>(sprites, sprite => sprite.name == "center_head");
-		m_sprites[(int)Parts.CenterBody] = System.Array.Find<Sprite>(sprites, sprite => sprite.name == "center_body");
-		m_sprites[(int)Parts.CenterTail] = System.Array.Find<Sprite>(sprites, sprite => sprite.name == "center_tail");
+		m_centerSprites[(int)CenterParts.Head] = System.Array.Find<Sprite>(sprites, sprite => sprite.name == "center_head");
+		m_centerSprites[(int)CenterParts.Body] = System.Array.Find<Sprite>(sprites, sprite => sprite.name == "center_body");
+		m_centerSprites[(int)CenterParts.Tail] = System.Array.Find<Sprite>(sprites, sprite => sprite.name == "center_tail");
 
 		m_pos = new Vector3(10, 10, 0);
-		m_dir = new Vector3(-1, 2, 0).normalized;
+		m_dir = new Vector3(-1, 1, 0).normalized;
 		m_speed = 4;
 		m_isBeforeReflected = false;
 	}
 
 	// Update is called once per frame
 	void Update () {
-
+#if false
 		GameObject obj, objCenter;
 		if( m_laser[m_laserIndex] == null )
 		{
@@ -163,7 +170,118 @@ public class LaserTest : MonoBehaviour {
 			m_laserCount = LaserLenMax;
 			m_laserIndexTop = (m_laserIndexTop + 1) % LaserLenMax;
 		}
-			
+#else
+		Vector3 pos, dir;
+		bool isBeforeReflected;
+		pos = m_pos;
+		dir = m_dir;
+		isBeforeReflected = m_isBeforeReflected;
+		update2(m_laser, m_sprites, ref pos, ref dir, ref isBeforeReflected, 1);
+
+		pos = m_pos;
+		dir = m_dir;
+		isBeforeReflected = m_isBeforeReflected;
+		update2(m_laserCenter, m_centerSprites, ref pos, ref dir, ref isBeforeReflected, 2);
+
+		m_pos = pos;
+		m_dir = dir;
+		m_isBeforeReflected = isBeforeReflected;
+
+		m_laserCount++;
+		m_laserIndex = (m_laserIndex + 1) % LaserLenMax;
+
+		if (m_laserCount >= LaserLenMax)
+		{
+			m_laserCount = LaserLenMax;
+			m_laserIndexTop = (m_laserIndexTop + 1) % LaserLenMax;
+		}
+
+#endif
+	}
+
+
+	void update2(GameObject[] laserObjs, Sprite[] sprites, ref Vector3 pos, ref Vector3 dir, ref bool isBeforeReflected, int order, bool isAdjust=false)
+	{
+		GameObject obj;
+		if (laserObjs[m_laserIndex] == null)
+		{
+			obj = GameObject.Instantiate<GameObject>(m_laserParts[(int)Parts.Head]);
+			laserObjs[m_laserIndex] = obj;
+			obj.transform.SetParent(gameObject.transform, false);
+		}
+
+		obj = laserObjs[m_laserIndex];
+		var spr = obj.GetComponent<SpriteRenderer>();
+		spr.sprite = sprites[(int)Parts.Head];
+
+		var angle = Vector3.Angle(new Vector3(1, 0, 0), m_dir);
+		var o = Vector3.Cross(new Vector3(1, 0, 0), m_dir);
+		if (o.z < 0)
+		{
+			angle = -angle;
+		}
+		obj.transform.rotation = Quaternion.AngleAxis(angle, new Vector3(0, 0, 1));
+
+		bool isReflected = false;
+		var after = pos + dir * m_speed;
+		Vector3 crossPos = Vector3.zero;
+		Vector3 n = Vector3.zero;
+		if (cross(pos, after, ref crossPos, ref n))
+		{
+			pos = crossPos;
+			dir = Vector3.Reflect(m_dir, n);
+			isReflected = true;
+		}
+		else
+		{
+			pos = after;
+		}
+
+		//	1つ前の種類をボディに変更
+		var prevIndex = m_laserIndex - 1;
+		if (prevIndex < 0)
+		{
+			prevIndex = LaserLenMax - 1;
+		}
+
+		var prevObj = laserObjs[prevIndex];
+		if (prevObj != null)
+		//if( prevObj != null && !isReflected )
+		{
+			if (!isBeforeReflected)
+			{
+				var prevSpr = prevObj.GetComponent<SpriteRenderer>();
+				prevSpr.sprite = sprites[(int)Parts.Body];
+			}
+			else if (isReflected)
+			{
+				var prevSpr = prevObj.GetComponent<SpriteRenderer>();
+				prevSpr.sprite = sprites[(int)Parts.Head];
+			}
+		}
+
+		//	反射後の1つ手前の絵を変えないようにするため状態を残す
+		isBeforeReflected = isReflected;
+
+		//	最後尾はテイルに変更
+		var lastIndex = (m_laserIndex + 1) % LaserLenMax;
+		var lastObj = laserObjs[lastIndex];
+		if (lastObj != null)
+		{
+			var lastSpr = lastObj.GetComponent<SpriteRenderer>();
+			lastSpr.sprite = sprites[(int)Parts.Tail];
+		}
+
+		//	描画が破綻しないようにオーダーを先頭から順番に割り振る
+		for (int i = 0; i < LaserLenMax; i++)
+		{
+			var index = (m_laserIndex + i) % LaserLenMax;
+			if (!laserObjs[index]) continue;
+			spr = laserObjs[index].GetComponent<SpriteRenderer>();
+			spr.sortingOrder = order;
+		}
+
+		obj.transform.position = pos;
 	}
 
 
