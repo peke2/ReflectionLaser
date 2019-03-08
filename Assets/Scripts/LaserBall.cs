f60cb7080ca3432bc6d2ae9e960d16933df5132c
peke2 <abem@dc4.so-net.ne.jp>
2019-03-08 23:29:29 +0900

    速度が変わっても全体の長さが変わらないようにするのは困難。 間を埋めるリソースが足りなければ短くなるし、反射の際にも長さは微妙に変わる。 そこまできっちり作っている暇は無いので、ある程度制限を考えるべきである。 ただ、小数点以下の数値が無い状態でCeilした際にずれが出るのだけはダメ。何とかしないといけない。

--- Assets/Scripts/LaserBall.cs
+++ Assets/Scripts/LaserBall.cs
@@ -20,6 +20,7 @@ public class LaserBall : MonoBehaviour {
 	Sprite[] m_sprites = new Sprite[(int)Parts.Max];
 
 	const int LaserLenMax = 16;
+	//const int LaserLenMax = 24;
 	GameObject[][] m_laserObjs = new GameObject[][] {
 		new GameObject[LaserLenMax],
 		new GameObject[LaserLenMax],
@@ -51,6 +52,7 @@ public class LaserBall : MonoBehaviour {
 		m_dir = m_direction.normalized;
 		m_isBeforeReflected = false;
 
+		//	リソースを用意
 		for (int i = 0; i < (int)Parts.Max; i++)
 		{
 			for(int j=0; j<LaserLenMax; j++)
@@ -69,115 +71,6 @@ public class LaserBall : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-#if false
-		GameObject obj, objCenter;
-		if( m_laser[m_laserIndex] == null )
-		{
-			obj = GameObject.Instantiate<GameObject>(m_laserParts[(int)Parts.Head]);
-			m_laser[m_laserIndex] = obj;
-			obj.transform.SetParent(gameObject.transform, false);
-
-			objCenter = GameObject.Instantiate<GameObject>(m_laserParts[(int)Parts.Head]);
-			m_laserCenter[m_laserIndex] = objCenter;
-			objCenter.transform.SetParent(gameObject.transform, false);
-		}
-
-		obj = m_laser[m_laserIndex];
-		var spr = obj.GetComponent<SpriteRenderer>();
-		spr.sprite = m_sprites[(int)Parts.Head];
-
-		objCenter = m_laserCenter[m_laserIndex];
-		spr = objCenter.GetComponent<SpriteRenderer>();
-		spr.sprite = m_sprites[(int)Parts.CenterHead];
-
-		var angle = Vector3.Angle(new Vector3(1, 0, 0), m_dir);
-		var o = Vector3.Cross(new Vector3(1, 0, 0), m_dir);
-		if (o.z < 0)
-		{
-			angle = -angle;
-		}
-		obj.transform.rotation = Quaternion.AngleAxis(angle, new Vector3(0,0,1));
-		objCenter.transform.rotation = Quaternion.AngleAxis(angle, new Vector3(0, 0, 1));
-
-		bool isReflected = false;
-		var after = m_pos + m_dir * m_speed;
-		Vector3 crossPos = Vector3.zero;
-		Vector3 n = Vector3.zero;
-		if (cross(m_pos, after, ref crossPos, ref n))
-		{
-			m_pos = crossPos;
-			m_dir = Vector3.Reflect(m_dir, n);
-			isReflected = true;
-		}
-		else
-		{
-			m_pos = after;
-		}
-
-		//	1つ前の種類をボディに変更
-		var prevIndex = m_laserIndex - 1;
-		if (prevIndex < 0)
-		{
-			prevIndex = LaserLenMax - 1;
-		}
-
-		var prevObj = m_laser[prevIndex];
-		if (prevObj != null && !m_isBeforeReflected)
-		{
-			var prevSpr = prevObj.GetComponent<SpriteRenderer>();
-			prevSpr.sprite = m_sprites[(int)Parts.Body];
-		}
-
-		var prevObjCenter = m_laserCenter[prevIndex];
-		if (prevObjCenter != null && !m_isBeforeReflected)
-		{
-			var prevSpr = prevObjCenter.GetComponent<SpriteRenderer>();
-			prevSpr.sprite = m_sprites[(int)Parts.CenterBody];
-		}
-
-		//	反射後の1つ手前の絵を変えないようにするため状態を残す
-		m_isBeforeReflected = isReflected;
-
-		//	最後尾はテイルに変更
-		var lastIndex = (m_laserIndex + 1) % LaserLenMax;
-		var lastObj = m_laser[lastIndex];
-		if (lastObj != null)
-		{
-			var lastSpr = lastObj.GetComponent<SpriteRenderer>();
-			lastSpr.sprite = m_sprites[(int)Parts.Tail];
-		}
-
-		var lastObjCenter = m_laserCenter[lastIndex];
-		if (lastObjCenter != null)
-		{
-			var lastSpr = lastObjCenter.GetComponent<SpriteRenderer>();
-			lastSpr.sprite = m_sprites[(int)Parts.CenterTail];
-		}
-
-		//	描画が破綻しないようにオーダーを先頭から順番に割り振る
-		for (int i=0; i<LaserLenMax; i++)
-		{
-			var index = (m_laserIndex + i) % LaserLenMax;
-			if (!m_laser[index]) continue;
-			spr = m_laser[index].GetComponent<SpriteRenderer>();
-			spr.sortingOrder = i;
-
-			spr = m_laserCenter[index].GetComponent<SpriteRenderer>();
-			spr.sortingOrder = i+LaserLenMax;
-		}
-
-		obj.transform.position = m_pos;
-		objCenter.transform.position = m_pos;
-
-		m_laserCount++;
-		m_laserIndex = (m_laserIndex + 1) % LaserLenMax;
-		
-		if( m_laserCount >= LaserLenMax )
-		{
-			m_laserCount = LaserLenMax;
-			m_laserIndexTop = (m_laserIndexTop + 1) % LaserLenMax;
-		}
-#else
 		Vector3 pos, dir;
 		pos = m_pos;
 		dir = m_dir;
@@ -186,7 +79,7 @@ public class LaserBall : MonoBehaviour {
 		m_pos = pos;
 		m_dir = dir;
 
-		/*m_laserCount++;
+	/*	m_laserCount++;
 		m_laserIndex = (m_laserIndex + 1) % LaserLenMax;
 
 		if (m_laserCount >= LaserLenMax)
@@ -194,96 +87,55 @@ public class LaserBall : MonoBehaviour {
 			m_laserCount = LaserLenMax;
 			m_laserIndexTop = (m_laserIndexTop + 1) % LaserLenMax;
 		}*/
-
-#endif
 	}
 
 
 	void update2(Sprite[] sprites, ref Vector3 pos, ref Vector3 dir, int order)
 	{
+		//	次の移動座標を算出
 		var after = pos + dir * m_speed;
 		Vector3 crossPos = Vector3.zero;
 		Vector3 n = Vector3.zero;
 		if (cross(pos, after, ref crossPos, ref n))
 		{
-			pos = crossPos;
+			after = crossPos;
 			dir = Vector3.Reflect(m_dir, n);
 		}
-		else
-		{
-			pos = after;
-		}
-
-		//	描画が破綻しないようにオーダーを先頭から順番に割り振る
-		/*for (int i = 0; i < LaserLenMax; i++)
-		{
-			var index = (m_laserIndex + i) % LaserLenMax;
-			if (!laserObjs[index]) continue;
-			spr = laserObjs[index].GetComponent<SpriteRenderer>();
-			spr.sortingOrder = order;
-		}*/
 		/*
 		for (int i = 0; i < (int)Parts.Max; i++)
 		{
-			GameObject obj;
-			if (m_laserObjs[i][m_laserIndex] == null)
-			{
-				obj = GameObject.Instantiate<GameObject>(m_laserParts[0]);
-				m_laserObjs[i][m_laserIndex] = obj;
-				obj.transform.SetParent(gameObject.transform, false);
-				var spr = obj.GetComponent<SpriteRenderer>();
-				spr.sprite = sprites[(int)Parts.Ball_0+i];
-				spr.sortingOrder = (int)Parts.Max - i;
-			}
-			else
-			{
-				obj = m_laserObjs[i][m_laserIndex];
-			}
+			var obj = m_laserObjs[i][m_laserIndex];
 			obj.transform.position = pos;
 		}*/
 
+		//	今の先頭との距離を出す
+		var diff = after - pos;
+		float num = diff.magnitude / MAX_DISTANCE;
+		var resCount = (int)Mathf.Ceil(diff.magnitude / MAX_DISTANCE);
 
-		var p0 = m_laserObjs[0][0].transform.position;
-		Vector3 p;
-		if( (pos - p0).magnitude > MAX_DISTANCE)
+		if( resCount == 2 )
 		{
-			p = pos - (pos - p0).normalized * MAX_DISTANCE;
-		}
-		else
-		{
-			p = p0;
+			Debug.Log("");
 		}
 
-		for (int i = 0; i < (int)Parts.Max; i++)
+		//	最大間隔が何個分か求める
+		//	リソースの最大数を超えない数に抑える
+		resCount = Mathf.Clamp(resCount, 1, LaserLenMax);
+
+		var d = diff.normalized;
+
+		//	一番遠い距離からリソースと座標を割り当てる
+		for(int i=resCount-1; i>=0; i--)
 		{
-			m_laserObjs[i][0].transform.position = pos;
+			var p = after - d * MAX_DISTANCE * i;
+			m_laserObjs[0][m_laserIndexTop].transform.position = p;
+			m_laserObjs[1][m_laserIndexTop].transform.position = p;
+			m_laserObjs[2][m_laserIndexTop].transform.position = p;
+			m_laserObjs[3][m_laserIndexTop].transform.position = p;
+			m_laserIndexTop = (m_laserIndexTop + 1) % LaserLenMax;
 		}
 
-		for (int i = 0; i < (int)Parts.Max; i++)
-		{
-			m_laserObjs[i][1].transform.position = p;
-		}
-
-		for (int i = 0; i < (int)Parts.Max; i++)
-		{
-			for (int j = LaserLenMax-1; j > 1 ; j--)
-			{
-				//var p0 = m_laserObjs[i][j].transform.position;
-				var p1 = m_laserObjs[i][j-1].transform.position;
-				var p2 = m_laserObjs[i][j-2].transform.position;
-
-				if ( (p2 - p1).magnitude > MAX_DISTANCE)
-				{
-					p = p2 - (p2 - p1).normalized * MAX_DISTANCE;
-				}
-				else
-				{
-					p = p1;
-				}
-				m_laserObjs[i][j].transform.position = p;
-			}
-		}
-
+		pos = after;
 	}
 
 
